31bb14dc37990aac2f846e5cc367dbff70056298
jadashipp <jada.l.shipp@gmail.com>
2020-03-02 14:16:22 -0600

    Corrections of one through four

--- 4.8.3_numpy_exercises.py
+++ 4.8.3_numpy_exercises.py
@@ -3,7 +3,7 @@ a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 
 # 1. How many negative numbers are there?
 mask = a < 0
-a[mask].sum()
+mask.sum()
 
 # 2. How many positive numbers are there?
 mask = a > 0
@@ -12,9 +12,12 @@ a[mask].sum()
 # 3. How many even positive numbers are there?
 a = a[a > 0]
 a = a[a % 2 == 0]
+a.size
 
 # 4. If you were to add 3 to each data point, 
 # how many positive numbers would there be?
-mask = a + 3
-mask = a > 0
-mask.sum()
+
+a2 = a + 3
+mask = a2 >0
+mask.sum
+
