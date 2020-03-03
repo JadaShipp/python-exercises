6bf58c55c90e14362b67bafbf9f0fc4eed449ff0
jadashipp <jada.l.shipp@gmail.com>
2020-03-03 15:51:50 -0600

    Exercise 3 complete. Starting Exercise 4

--- 4.8.3_numpy_exercises.py
+++ 4.8.3_numpy_exercises.py
@@ -21,3 +21,20 @@ a2 = a + 3
 mask = a2 >0
 mask.sum
 
+ # 5. If you squared each number, 
+ # what would the new mean and standard deviation be?
+ 
+ a3 = a**2
+ a3.mean()
+ a3.std()
+
+# 6. A common statistical operation on a dataset is centering. 
+# This means to adjust the data such that the center of the data is at 0. 
+# This is done by subtracting the mean from each data point. 
+# Center the data set.
+a.mean()
+a2 = (a - a.mean())
+
+# 7 Calculate the z-score for each data point. 
+
+a3 = a2 / a.std() 
